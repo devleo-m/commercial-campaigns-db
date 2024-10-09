ede33e6b22bf7ef7b65be833115a026c102704a6
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-10-09 15:46:12 -0300

    Create all Interfaces

---
+++ src/interface/user.interface.ts
@@ -0,0 +1,11 @@
+import { Model } from 'sequelize'
+
+export interface IUser extends Model {
+    id: number
+    name: string
+    email: string
+    password: string
+    readonly createdAt: Date
+    readonly updatedAt: Date
+    readonly deletedAt?: Date
+}
